eba55b291dc6cfab35f1cff2f6d2d094f4a7caff
antonio <antoniobvg@gmail.com>
2013-02-09 14:22:20 -0200

    cpw disse que vai vir esse hook no prox forge

--- items/SonicBoots.java
+++ items/SonicBoots.java
@@ -1,8 +1,12 @@
 /*    */ package FunMod.items;
 /*    */ 
-/*    */ import net.minecraft.item.EnumArmorMaterial;
+/*    */ import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IArmorTextureProvider;
 import FunMod.FunMod;
 /*    */ public class SonicBoots extends ItemArmor
@@ -15,16 +19,7 @@ import FunMod.FunMod;
 /* 15 */     this.iconIndex = 25;
 /*    */     this.setCreativeTab(FunMod.funmodtab);
 }
-/*    */ /**
-/*       public String getArmorTextureFile(ItemStack par1)
-/*       {
-/* 21      if ((par1.itemID == ExtremeMinerios.alexHelmet.shiftedIndex) || (par1.itemID == ExtremeMinerios.alexPlate.shiftedIndex) || 
-/* 22        (par1.itemID == ExtremeMinerios.alexBoots.shiftedIndex))
-/* 23  "/ExtremeMinerios/client/armaduras/alex_1.png";
-/* 24/     if (par1.itemID == ExtremeMinerios.alexBoots.shiftedIndex)
-/* 25        return "/ExtremeMinerios/client/armaduras/enx_bo.png";
-/* 26      return "/ExtremeMinerios/client/armaduras/alex_2.png";
-/*   */   
+
        public String getTextureFile() {
      return "/FunMod/cliente/texturas/texturas.png";
        }     
@@ -35,9 +30,14 @@ public String getArmorTextureFile(ItemStack par1) {
 		        return "/FunMod/cliente/texturas/armaduras/sonic_2.png";
 	 return "/FunMod/cliente/texturas/armaduras/sonic_2.png";
 }
+
+public void onArmorTickUpdate(World world, EntityPlayer player, ItemStack itemStack2)
+
+{
+ if(itemStack2.getItem().itemID == this.itemID){
+	 player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 10));
+ }
+
+ }
 }
 
-/* Location:           N:\a\
- * Qualified Name:     ExtremeMinerios.ItealexBoots
- * JD-Core Version:    0.6.1
- */
